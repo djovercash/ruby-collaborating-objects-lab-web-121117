8850ae947f152c2a69c063cb0842cbfdc26c96dd
github username <github email address>
2017-12-15 16:51:15 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,6 @@ class Song
   def artist_name=(artist_name)
     self.artist = Artist.find_or_create_by_name(artist_name.name)
     self.artist.add_song(self)
-    binding.pry
   end
 
   def self.new_by_filename(file_name)
